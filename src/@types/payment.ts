ad8ecb6884f32ebad51ba0159157b623f087603b
Czino <eric@czino.de>
2023-09-29 15:29:15 +0200

    Update payment method types

--- src/@types/payment.ts
+++ src/@types/payment.ts
@@ -1,33 +1,20 @@
 import { BitcoinEvent } from './events'
 import { Country, Currency } from './global'
 
-export type PaymentMethod =
-  | 'sepa'
-  | 'instantSepa'
-  | 'paypal'
-  | 'revolut'
+export type EuPaymentMethods =
+  | 'advcash'
   | 'applePay'
-  | 'wise'
-  | 'twint'
-  | 'swish'
-  | 'mbWay'
   | 'bizum'
-  | 'satispay'
-  | 'vipps'
-  | 'fasterPayments'
-  | 'mobilePay'
-  | 'skrill'
-  | 'neteller'
-  | 'paysera'
-  | 'lydia'
   | 'blik'
-  | 'straksbetaling'
-  | 'keksPay'
+  | 'fasterPayments'
   | 'friends24'
-  | 'n26'
+  | 'instantSepa'
   | 'iris'
-  | 'verse'
-  | 'paylib'
+  | 'keksPay'
+  | 'lydia'
+  | 'mbWay'
+  | 'mobilePay'
+  | 'n26'
   | 'nationalTransferBG'
   | 'nationalTransferCZ'
   | 'nationalTransferDK'
@@ -35,11 +22,68 @@ export type PaymentMethod =
   | 'nationalTransferNO'
   | 'nationalTransferPL'
   | 'nationalTransferRO'
-  | 'giftCard.amazon'
-  | `giftCard.amazon.${Country}`
-  | 'liquid.usdt'
-  | 'cash'
-  | `cash.${BitcoinEvent['id']}`
+  | 'nationalTransferTR'
+  | 'nationalTransferIS'
+  | 'nationalTransferCH'
+  | 'nationalTransferSE'
+  | 'neteller'
+  | 'papara'
+  | 'paylib'
+  | 'paypal'
+  | 'paysera'
+  | 'revolut'
+  | 'satispay'
+  | 'sepa'
+  | 'skrill'
+  | 'straksbetaling'
+  | 'swish'
+  | 'twint'
+  | 'verse'
+  | 'vipps'
+  | 'wise'
+  | 'bankera'
+  | 'rebellion'
+  | 'postePay'
+export type LatAmPaymentMethods =
+  | 'alias'
+  | 'bancolombia'
+  | 'cbu'
+  | 'cvu'
+  | 'mercadoPago'
+  | 'nequi'
+  | 'rappipay'
+  | 'sinpe'
+  | 'sinpeMovil'
+  | 'pix'
+
+export type AfricaPaymentMethods =
+  | 'airtelMoney'
+  | 'chippercash'
+  | 'eversend'
+  | 'm-pesa'
+  | 'moov'
+  | 'mtn'
+  | 'nationalTransferNG'
+  | 'orangeMoney'
+  | 'wave'
+  | 'payday'
+  | 'flutterwave'
+  | 'mobileAirtime'
+  | 'klasha'
+  | 'accrue'
+  | 'wirepay'
+
+export type BitcoinPaymentMethods = 'liquid' | 'lnurl'
+export type InternationalPaymentMethds = 'giftCard.amazon' | `giftCard.amazon.${Country}`
+export type CashPaymentMethds = `cash.${BitcoinEvent['id']}`
+
+export type PaymentMethod =
+  | EuPaymentMethods
+  | LatAmPaymentMethods
+  | AfricaPaymentMethods
+  | InternationalPaymentMethds
+  | BitcoinPaymentMethods
+  | CashPaymentMethds
 
 export type PaymentMethodInfo = {
   id: PaymentMethod
