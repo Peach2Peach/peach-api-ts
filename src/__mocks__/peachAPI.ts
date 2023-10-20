2181c216403f3da530d852070945c198af764f97
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-10-20 12:49:23 +0200

    Added setPeachAccount to mock

--- src/__mocks__/peachAPI.ts
+++ src/__mocks__/peachAPI.ts
@@ -1,7 +1,10 @@
+import { BIP32Interface } from 'bip32'
+
 export const peachAPI = () => ({
   private: {
     contract: {
       acknowledgeDispute: () => Promise.resolve({ result: { success: true }, error: null }),
     },
   },
+  setPeachAccount: (peachAccount: BIP32Interface) => peachAccount,
 })
