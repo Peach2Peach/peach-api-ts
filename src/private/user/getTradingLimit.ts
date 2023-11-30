13f5089c78302291c20514cb09c3dabd5f178b09
Michael Manke <66833819+MManke188@users.noreply.github.com>
2023-11-30 18:13:34 +0100

    Updated getTradingLimit

--- src/private/user/getTradingLimit.ts
+++ src/private/user/getTradingLimit.ts
@@ -7,27 +7,29 @@ import {
 } from '../../@types/api/userAPI'
 import { parseResponse } from '../../helpers/parseResponse'
 import { PeachAPIHelpers, PeachAPIOptions, RequestProps } from '../../types'
-import { getResult } from '../../utils/result'
 
 type Props = RequestProps & GetTradingLimitRequestParams & GetTradingLimitRequestQuery & GetTradingLimitRequestBody
 
-export const getTradingLimit
-  = ({ url }: PeachAPIOptions, helpers: PeachAPIHelpers) =>
-    async ({ signal }: Props = {}) => {
-      const response = await fetch(`${url}/v1/user/tradingLimit`, {
-        headers: helpers.getPrivateHeaders(url),
-        method: 'GET',
-        signal,
-      })
+export const getTradingLimit =
+  ({ url }: PeachAPIOptions, helpers: PeachAPIHelpers) =>
+  async ({ signal }: Props = {}) => {
+    const response = await fetch(`${url}/v1/user/tradingLimit`, {
+      headers: helpers.getPrivateHeaders(url),
+      method: 'GET',
+      signal,
+    })
 
-      const parsedResponse = await parseResponse<GetTradingLimitResponseBody, GetTradingLimitErrorResponseBody>(response)
+    const parsedResponse = await parseResponse<GetTradingLimitResponseBody, GetTradingLimitErrorResponseBody>(response)
+    const { result } = parsedResponse
 
-      if (!parsedResponse.isOk()) return parsedResponse
-
-      const result = parsedResponse.getValue()
-      if (result.daily === null) result.daily = Infinity
-      if (result.monthlyAnonymous === null) result.monthlyAnonymous = Infinity
-      if (result.yearly === null) result.yearly = Infinity
-
-      return getResult(result)
+    if (!result) return parsedResponse
+    return {
+      ...parsedResponse,
+      result: {
+        ...result,
+        daily: result?.daily ?? Infinity,
+        monthlyAnonymous: result?.monthlyAnonymous ?? Infinity,
+        yearly: result?.yearly ?? Infinity,
+      },
     }
+  }
