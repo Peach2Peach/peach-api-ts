a79ec75123cb3e1e4abd198d69ff6172b152abee
Czino <eric@czino.de>
2023-12-19 21:09:48 +0100

    Make param optional

--- src/@types/api/offerAPI.ts
+++ src/@types/api/offerAPI.ts
@@ -126,7 +126,7 @@ export type MatchOfferRequestBody = {
   paymentMethod: PaymentMethod
   price: number
   premium: number
-  paymentData: OfferPaymentData
+  paymentData?: OfferPaymentData
   symmetricKeyEncrypted?: string
   symmetricKeySignature?: string
   paymentDataEncrypted?: string
