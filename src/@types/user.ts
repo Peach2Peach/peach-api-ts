45a042ff36568047924c6406d8d23dfcce4cd811
Czino <eric@czino.de>
2023-12-20 11:39:42 +0100

    Update user type

--- src/@types/user.ts
+++ src/@types/user.ts
@@ -33,6 +33,7 @@ export type User = {
   recentRating: number
   referralCode?: string
   referredTradingAmount: number
+  openedTrades: number
   trades: number
   uniqueId: string
   usedReferralCode?: string
