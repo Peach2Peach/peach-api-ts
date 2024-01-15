69f0b03448cf7fc061a4c07b2d8eaf02da81a5a3
Michael Manke <66833819+MManke188@users.noreply.github.com>
2024-01-15 18:11:16 +0100

    Added test match data

---
+++ src/testData/match.ts
@@ -0,0 +1,25 @@
+import { Match } from '../@types/match'
+import { defaultUser } from './user'
+
+export const match: Match = {
+  user: defaultUser,
+  offerId: 'offerId',
+  amount: 210000,
+  premium: 2.1,
+  prices: {
+    EUR: 210000,
+  },
+  creationDate: new Date('2009-01-03T18:15:05.000Z'),
+  lastModified: new Date('2009-01-03T18:15:05.000Z'),
+  instantTrade: false,
+  matched: false,
+  matchedPrice: null,
+  meansOfPayment: {
+    EUR: ['sepa'],
+  },
+  symmetricKeyEncrypted: 'symmetricKeyEncrypted',
+  symmetricKeySignature: 'symmetricKeySignature',
+  unavailable: {
+    exceedsLimit: [],
+  },
+}
