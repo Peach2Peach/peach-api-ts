9d943f1d97d38018940033819028c1c15ca91ec6
Czino <eric@czino.de>
2024-03-14 15:39:23 +0100

    Add Transaction type

--- src/@types/electrs-liquid.ts
+++ src/@types/electrs-liquid.ts
@@ -10,3 +10,46 @@ export type UTXO = {
   noncecommitment?: string,
   valuecommitment?: string,
 }
+
+
+export type Input = {
+  txid: string;
+  vout: number;
+  prevout?: {
+    scriptpubkey: string;
+    scriptpubkey_asm: string;
+    scriptpubkey_type: string;
+    scriptpubkey_address?: string;
+    value: number;
+    asset: string;
+  };
+  scriptsig: string;
+  scriptsig_asm: string;
+  inner_redeemscript_asm?: string;
+  witness?: string[];
+  is_coinbase: boolean;
+  is_pegin: boolean;
+  sequence: number;
+};
+
+export type Output = {
+  scriptpubkey: string;
+  scriptpubkey_asm: string;
+  scriptpubkey_type: string;
+  scriptpubkey_address?: string;
+  value: number;
+  asset: string;
+};
+
+export type Transaction = {
+  txid: string;
+  version: number;
+  locktime: number;
+  vin: Input[];
+  vout: Output[];
+  size: number;
+  weight: number;
+  fee: number;
+  value?: number;
+  status: TransactionStatus;
+};
