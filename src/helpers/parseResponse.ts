d81f941a9690e9610ea081f9337bd7c76478755c
Michael Manke <michael@peachbitcoin.com>
2025-05-02 12:36:59 +0200

    Fixed return type

--- src/helpers/parseResponse.ts
+++ src/helpers/parseResponse.ts
@@ -1,7 +1,7 @@
 import { APIError } from "../@types/global";
 import { dateTimeReviver } from "../utils/json";
 import { getError, getResult, parseError } from "../utils/result";
-import { Result } from "../utils/result/types";
+import { Err, Result } from "../utils/result/types";
 import {
   RESPONSE_ERRORS,
   getResponseError,
@@ -11,28 +11,24 @@ import {
 export const parseResponse = async <R, E extends APIError<string | null>>(
   response: Response,
   string = false,
-): Promise<Result<R, E>> => {
+): Promise<Result<R, E> | Err<E, R>> => {
   try {
     const responseError = getResponseError(response);
     if (isErrorStatus(responseError))
-      // @ts-ignore
       return getError({ error: RESPONSE_ERRORS[responseError] } as E);
 
     const data = !string
       ? JSON.parse(await response.text(), dateTimeReviver)
       : await response.text();
 
-    // @ts-ignore
     if (response.status !== 200) return getError(data);
     return getResult(data);
   } catch (e) {
     const parsedError = parseError(e);
     if (parsedError.includes("JSON Parse error")) {
-      // @ts-ignore
       return getError({ error: RESPONSE_ERRORS[500] } as E);
     }
 
-    // @ts-ignore
     return getError({ error: RESPONSE_ERRORS[500] } as E);
   }
 };
