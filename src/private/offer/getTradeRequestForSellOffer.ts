558481e5ddafa6e4ca216e29fbc3efcd20a9fae7
Michael Manke <michael@peachbitcoin.com>
2025-06-17 13:37:23 +0200

    Added offer type specific getTradeRequest

---
+++ src/private/offer/getTradeRequestForSellOffer.ts
@@ -0,0 +1,24 @@
+import { APIError } from "../../@types/global";
+import { parseResponse } from "../../helpers/parseResponse";
+import { PeachAPIHelpers, PeachAPIOptions, RequestProps } from "../../types";
+import { TradeRequestForSellOffer } from "./getTradeRequestsForSellOffer";
+
+type Props = RequestProps & { offerId: string; requestingOfferId?: string };
+
+type Response = (TradeRequestForSellOffer & { amount: number }) | null;
+
+export const getTradeRequestForSellOffer =
+  ({ url }: PeachAPIOptions, helpers: PeachAPIHelpers) =>
+  async ({ offerId, requestingOfferId, signal }: Props) => {
+    let route = `${url}/v1/offer/${offerId}/tradeRequest`;
+    if (requestingOfferId) {
+      route += `/${requestingOfferId}`;
+    }
+    const response = await fetch(route, {
+      headers: helpers.getPrivateHeaders(url),
+      method: "GET",
+      signal,
+    });
+
+    return parseResponse<Response, APIError<"NOT_FOUND">>(response);
+  };
