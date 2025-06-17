5286c634a54cbf868c634ce969f483604287883f
Michael Manke <michael@peachbitcoin.com>
2025-06-17 13:39:59 +0200

    Added new getOfferPreferences request

---
+++ src/private/offer/getBuyOfferPreferences.ts
@@ -0,0 +1,28 @@
+import { APIError } from "../../@types/global";
+import { MeansOfPayment } from "../../@types/payment";
+import { parseResponse } from "../../helpers/parseResponse";
+import { PeachAPIHelpers, PeachAPIOptions, RequestProps } from "../../types";
+
+type Props = RequestProps & { offerId: string };
+
+type Res = {
+  amount: [number, number];
+  meansOfPayment: MeansOfPayment;
+  maxPremium: number | undefined;
+  minReputation: number | undefined;
+};
+
+export const getBuyOfferPreferences =
+  ({ url }: PeachAPIOptions, helpers: PeachAPIHelpers) =>
+  async ({ offerId, signal }: Props) => {
+    const response = await fetch(`${url}/v1/offer/${offerId}/buy/details`, {
+      headers: helpers.getPrivateHeaders(url),
+      method: "GET",
+      signal,
+    });
+
+    return parseResponse<
+      Res,
+      APIError<"NOT_FOUND" | "AUTHENTICATION_FAILED" | "UNAUTHORIZED">
+    >(response);
+  };
---
+++ src/private/offer/getSellOfferPreferences.ts
@@ -0,0 +1,26 @@
+import { APIError } from "../../@types/global";
+import { MeansOfPayment } from "../../@types/payment";
+import { parseResponse } from "../../helpers/parseResponse";
+import { PeachAPIHelpers, PeachAPIOptions, RequestProps } from "../../types";
+
+type Props = RequestProps & { offerId: string };
+type Res = {
+  amount: number;
+  premium: number;
+  meansOfPayment: MeansOfPayment;
+};
+
+export const getSellOfferPreferences =
+  ({ url }: PeachAPIOptions, helpers: PeachAPIHelpers) =>
+  async ({ offerId, signal }: Props) => {
+    const response = await fetch(`${url}/v1/offer/${offerId}/sell/details`, {
+      headers: helpers.getPrivateHeaders(url),
+      method: "GET",
+      signal,
+    });
+
+    return parseResponse<
+      Res,
+      APIError<"NOT_FOUND" | "AUTHENTICATION_FAILED" | "UNAUTHORIZED">
+    >(response);
+  };
