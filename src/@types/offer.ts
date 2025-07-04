cbcbb90ea04e80fda25fb9be8b1324b93b307a4a
alexbrou <alexandrepilarbrou@gmail.com>
2025-07-04 14:57:18 +0100

    add new types of TradeStatus

--- src/@types/offer.ts
+++ src/@types/offer.ts
@@ -34,7 +34,9 @@ export type TradeStatus =
   | "releaseEscrow"
   | "searchingForPeer"
   | "tradeCanceled"
-  | "tradeCompleted";
+  | "tradeCompleted"
+  | "fundingExpired"
+  | "createEscrow";
 
 export type PaymentMethodCountry =
   | "BG"
