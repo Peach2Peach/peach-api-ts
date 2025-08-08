055b3a76f9dc0c366d18d03e8975b0d4ce00925d
alexbrou <alexandrepilarbrou@gmail.com>
2025-08-08 11:26:17 +0100

    add submit user bitcoin level api call

---
+++ src/private/user/submitUserBitcoinLevel.ts
@@ -0,0 +1,21 @@
+import { APIError, APISuccess } from "../../@types/global";
+import { parseResponse } from "../../helpers/parseResponse";
+import { PeachAPIHelpers, PeachAPIOptions, RequestProps } from "../../types";
+
+export type UserBitcoinLevel = "firstTime" | "firstTimeP2P" | "bitcoiner";
+type Props = RequestProps & { bitcoinLevel: UserBitcoinLevel };
+
+export const submitUserBitcoinLevel =
+  ({ url }: PeachAPIOptions, helpers: PeachAPIHelpers) =>
+  async ({ bitcoinLevel, signal }: Props) => {
+    const response = await fetch(`${url}/v1/user/bitcoinLevel`, {
+      headers: helpers.getPrivateHeaders(url),
+      method: "POST",
+      body: JSON.stringify({
+        bitcoinLevel,
+      }), 
+      signal,
+    });
+
+    return parseResponse<APISuccess, APIError<string | null>>(response);
+  };
