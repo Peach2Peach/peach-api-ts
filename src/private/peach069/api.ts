68b03e3d1d5307ed7d904108d140f55642690d1f
alexbrou <alexandrepilarbrou@gmail.com>
2026-03-20 16:07:24 +0000

    add getBlockedUsers endpoint

--- src/private/peach069/api.ts
+++ src/private/peach069/api.ts
@@ -5,6 +5,7 @@ import { canPerformInstantTradeWithBuyOfferById } from "./canPerformInstantTrade
 import { canPerformInstantTradeWithSellOfferById } from "./canPerformInstantTradeWithSellOfferById";
 import { createBuyOffer } from "./createBuyOffer";
 import { deleteBuyOfferById } from "./deleteBuyOfferById";
+import { getBlockedUsers } from "./getBlockedUsers";
 import { getBuyOfferById } from "./getBuyOfferById";
 import { getBuyOffers } from "./getBuyOffers";
 import { getBuyOfferTradeRequestPerformedById } from "./getBuyOfferTradeRequestPerformedById";
@@ -141,6 +142,10 @@ export const peach069API = (
     helpers,
   ),
   setEncryptedPaymentDataOnSelfUser69:setEncryptedPaymentDataOnSelfUser69(
+    options,
+    helpers,
+  ),
+  getBlockedUsers:getBlockedUsers(
     options,
     helpers,
   ),
